27f339e21a915783a81b63ece79435a4b0917528
Yovan Juggoo <yovan.juggoo@gmail.com>
2017-01-27 20:26:57 +0400

    Status code and location header

--- 001_First_Jersey_Project/src/main/java/com/yovan/firstjerseyproject/MessageResource.java
+++ 001_First_Jersey_Project/src/main/java/com/yovan/firstjerseyproject/MessageResource.java
@@ -1,5 +1,6 @@
 package com.yovan.firstjerseyproject;
 
+import java.net.URI;
 import java.util.List;
 
 import javax.ws.rs.BeanParam;
@@ -11,7 +12,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 import com.yovan.firstjerseyproject.beans.MessagesBeanParams;
 import com.yovan.firstjerseyproject.model.Message;
@@ -24,18 +28,19 @@ public class MessageResource {
 
 	MessageService messageService = new MessageService();
 
-	/*@GET
-	public List<Message> getMessage(
-			@QueryParam("year") int year, 
-			@QueryParam("start") int start,
-			@QueryParam("size") int size) {
-		if (year > 0)
-			return messageService.getMessages(year);
-		if (start >= 0 && size > 0)
-			return messageService.getMessages(start, size);
-		return messageService.getMessages();
-	}*/
-	
+	/*
+	 * @GET public List<Message> getMessage(
+	 * 
+	 * @QueryParam("year") int year,
+	 * 
+	 * @QueryParam("start") int start,
+	 * 
+	 * @QueryParam("size") int size) { if (year > 0) return
+	 * messageService.getMessages(year); if (start >= 0 && size > 0) return
+	 * messageService.getMessages(start, size); return
+	 * messageService.getMessages(); }
+	 */
+
 	@GET
 	public List<Message> getMessages(@BeanParam MessagesBeanParams beanParams) {
 		if (beanParams.getYear() > 0)
@@ -51,9 +56,32 @@ public class MessageResource {
 		return messageService.getMessage(messageId);
 	}
 
+	/*
+	 * @POST public Message addMessage(Message message) { return
+	 * messageService.addMessage(message); }
+	 */
+
+	/*
+	 * @POST public Response addMessage(Message message) { message =
+	 * messageService.addMessage(message); return
+	 * Response.status(Status.CREATED) .entity(message) .build(); }
+	 */
+
+	/*
+	 * @POST public Response addMessage(Message message) { message =
+	 * messageService.addMessage(message); try { return Response.created(new
+	 * URI("/001_First_Jersey_Project/webapi/messages/" + message.getId()))
+	 * .entity(message) .build(); } catch (URISyntaxException e) {
+	 * e.printStackTrace(); return null; } }
+	 */
+
 	@POST
-	public Message addMessage(Message message) {
-		return messageService.addMessage(message);
+	public Response addMessage(Message message, @Context UriInfo uriInfo) {
+		message = messageService.addMessage(message);
+		URI uri = uriInfo.getAbsolutePathBuilder()
+				.path(String.valueOf(message.getId()))
+				.build();
+		return Response.created(uri).entity(message).build();
 	}
 
 	@PUT
@@ -68,19 +96,18 @@ public class MessageResource {
 	public void removeMessage(@PathParam("messageId") long messageId) {
 		messageService.removeMessage(messageId);
 	}
-	
-	/*@GET
-	@Path("/{messageId}/comments")
-	public List<Comment> getComments(@PathParam("messageId") long messageId){
-		List<Comment> commentsList = new ArrayList<>();
-		return commentsList;
-	}*/
-	
+
+	/*
+	 * @GET
+	 * 
+	 * @Path("/{messageId}/comments") public List<Comment>
+	 * getComments(@PathParam("messageId") long messageId){ List<Comment>
+	 * commentsList = new ArrayList<>(); return commentsList; }
+	 */
+
 	@Path("/messagesId/comments")
-	public CommentResource getCommentResource(){
+	public CommentResource getCommentResource() {
 		return new CommentResource();
 	}
-	
-	
-	
+
 }
