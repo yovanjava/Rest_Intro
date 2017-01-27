43683eedc48d7ac30e56bba72cf043eaf9613061
Yovan Juggoo <yovan.juggoo.php@gmail.com>
2017-01-27 11:28:29 +0400

    Filtering and pagination

--- 001_First_Jersey_Project/src/main/java/com/yovan/firstjerseyproject/service/MessageService.java
+++ 001_First_Jersey_Project/src/main/java/com/yovan/firstjerseyproject/service/MessageService.java
@@ -1,6 +1,7 @@
 package com.yovan.firstjerseyproject.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -16,11 +17,29 @@ public class MessageService {
 		messagesMap.put(1L, new Message(1, "This is a new message", "john", new Date()));
 		messagesMap.put(2L, new Message(2, "This is the second message", "paul", new Date()));
 	}
-	
+
 	public List<Message> getMessages() {
 		return new ArrayList<>(messagesMap.values());
 	}
-	
+
+	public List<Message> getMessages(int year) {
+		List<Message> filteredMessages = new ArrayList<>();
+		Calendar calendar = Calendar.getInstance();
+		for (Message message : messagesMap.values()) {
+			calendar.setTime(message.getCreatedAt());
+			if(calendar.get(Calendar.YEAR) == year)
+				filteredMessages.add(message);
+		}
+		return filteredMessages;
+	}
+
+	public List<Message> getMessages(int start, int size) {
+		List<Message> filteredMessages = new ArrayList<>(messagesMap.values());
+		if((start + size) > filteredMessages.size())
+			return new ArrayList<>();
+		return filteredMessages.subList(start, start + size);
+	}
+
 	public Message getMessage(Long id) {
 		return messagesMap.get(id);
 	}
